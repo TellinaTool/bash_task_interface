f52a0164cfb0dc3b2b611fae09c47809e8bf3b54
Deric Pang <dericp@cs.washington.edu>
2017-10-23 22:05:50 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# bash_task_interface
+
+Clone the project to `~/workspace/tellina-user-study-system` on your machine.
+
+Run `source init.sh` which will set up the study directories and install bash-preexec (https://github.com/rcaloras/bash-preexec).
+
+Change to a task directory: `cd study/task_1`.
+
+Print the task prompt with `p`.
+
+Reset the task directories with `r`.
---
+++ init.sh
@@ -0,0 +1,31 @@
+# Installing Bash preexec.
+curl https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec.sh -o ~/.bash-preexec.sh
+source ~/.bash-preexec.sh
+preexec() {
+    echo "pre-exec"
+}
+# This function is where we need to verify the output of the command.
+# Right now, the task passes if you create a file called "pass".
+precmd() {
+    TASK_NUM=${PWD##*/}
+
+    if [[ $TASK_NUM == task_* ]]; then
+        if [[ -e pass ]]; then
+            echo "$TASK_NUM passed!"
+        else
+            echo "$TASK_NUM not passed."
+            echo "TODO: print diff"
+        fi
+    fi
+}
+
+export WORKING_DIR="$HOME/workspace/tellina-user-study-system"
+export STUDY_DIR="study"
+export STUDY_BACKUP="$HOME/workspace/tellina-user-study-system/class-website-template-master"
+
+export PATH=$WORKING_DIR/bin:$PATH
+
+alias r="cd $WORKING_DIR && reset"
+alias p="prompt"
+
+reset
