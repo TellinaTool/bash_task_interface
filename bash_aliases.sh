daf8103a925f935c627857f66f8ed6d1fb495268
rajas <rajas@cs.washington.edu>
2017-11-12 18:49:03 -0800

    added a toggle command to remove the task message

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -7,18 +7,27 @@ export TASK_DIR="$HOME/task"
 export CURR_TASK="$REPO_DIR/task_progress/curr_task"
 # prepend task_interface scripts to the user's path
 export PATH=$REPO_DIR/bin:$PATH
+# allows the diff and task number outputs to be turned off
+export DIFF_MODE=1
 
 alias reset="cd $HOME && cd $TASK_DIR"
+alias toggle="source toggle-diff-mode"
+
 
 # Installing Bash preexec.
 curl https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec.sh -o ~/.bash-preexec.sh
 source ~/.bash-preexec.sh
 preexec() {
-    TASK_NUM=`cat $CURR_TASK`
-    PREV_COMMAND=$1
-    $REPO_DIR/scripts/verify_task.py $TASK_NUM $PREV_COMMAND
-  }
+	# TODO this needs to be disabled for commands like vi since it hangs
+	if [ $DIFF_MODE = 1 ] ; then
+    		TASK_NUM=`cat $CURR_TASK`
+    		PREV_COMMAND=$1
+    		$REPO_DIR/scripts/verify_task.py $TASK_NUM $PREV_COMMAND
+	fi
+}
 
 precmd() {
-    task
-  }
+	if [ $DIFF_MODE = 1 ] ; then
+		task
+	fi
+}
