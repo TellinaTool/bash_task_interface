785bc02b90e1106e25c4ee3caf0d706fbacff9c3
Deric Pang <dericp@cs.washington.edu>
2017-11-26 18:13:31 -0800

    adding task abandoning and time limits

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -12,27 +12,41 @@ export DIFF_MODE=1
 
 alias reset="cd $HOME && reset && cd $TASK_DIR"
 alias toggle="source toggle-diff-mode"
-
+alias abandon="source abandon-task"
 
 # Installing Bash preexec.
 curl https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec.sh -o ~/.bash-preexec.sh
 source ~/.bash-preexec.sh
 preexec() {
     # TODO this needs to be disabled for commands like vi since it hangs
-    # If we want to see output and the we're not running the diff command, then verify the command.
+    # Store the last command issued by the user into a file.
     echo $1 > $REPO_DIR/user_output/prev_cmd
 }
 
 precmd() {
+    # The previous command issued by the user.
     PREV_CMD=`cat $REPO_DIR/user_output/prev_cmd`
+    # if the previous command was not a diff command, reset command, or a task command
     if [ "$PREV_CMD" != "diff" ] && [ "$PREV_CMD" != "reset" ] && [ "$PREV_CMD" != "task" ]; then
         TASK_NUM=`cat $CURR_TASK`
-        $REPO_DIR/scripts/verify_task.py $TASK_NUM $PREV_CMD
+        # Verify the output of the previous command.
+        $REPO_DIR/scripts/verify_task.py $TASK_NUM $SECONDS $PREV_CMD 
         EXIT=$?
+        # if the user passes the task
         if [ "$EXIT" = 1 ]; then
             reset
             echo "-----------------------"
             echo "You have passed task $TASK_NUM!"
+            # close the current meld window
+            pkill meld
+            if [ "$TASK_NUM" = 22 ]; then
+                echo "Congratulations! You have finished the study. Go ahead and log out."
+            fi
+        # else, if the user has gone over time for this task
+        elif [ "$EXIT" = 2 ]; then
+            abandon
+            echo "-----------------------------------"
+            echo "You have run out of time on task $TASK_NUM."
             # close the current meld window
             pkill meld
             if [ "$TASK_NUM" = 22 ]; then
--- scripts/verify_task.py
+++ scripts/verify_task.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
+"""
+There are three exit codes returned from this script.
+
+If the user has passed the task, returns 1.
+If the user has not passed the task and has run out of time for the current
+task, returns 2.
+
+If the user does not pass the task, returns 0.
+"""
+
 from __future__ import print_function
 import sys
 import os
@@ -8,34 +18,56 @@ import filecmp
 import tarfile
 
 FILESYSTEM_TASKS = {2, 3, 4, 5, 6, 11, 12, 15, 20, 22}
+TASK_TIME_LIMIT = 0.1
+
 
 def main():
+    # the current task number, as a str
     task_num = sys.argv[1]
-    command = ' '.join(sys.argv[2:])
+    # the number of seconds elapsed since the beginning of the task, as a str
+    seconds = sys.argv[2]
+    # the current command
+    command = ' '.join(sys.argv[3:])
     try:
+        # the file where we will save the raw output of the command
         output_path = os.environ['REPO_DIR'] + '/user_output/out'
         output_file = open(output_path, 'w')
 
         devnull = open(os.devnull, 'wb')
         if int(task_num) in FILESYSTEM_TASKS:
+            # if the task is a filesytem task, get the files in the current filesystem
             ret = subprocess.call('find .', shell=True, stderr=devnull, stdout=output_file)
         else:
+            # otherwise, get the stdout of the command
             ret = subprocess.call(command, shell=True, stderr=devnull, stdout=output_file)
 
         # close output file for normalization
         output_file.close()
 
         norm_out_path = os.environ['REPO_DIR'] + '/user_output/norm_out'
-
         normalize_output(output_path, norm_out_path, task_num)
 
+        # if the task was passed
         if verify(norm_out_path, task_num):
             to_next_task(task_num)
+            reset_task_time()
+            # return exit code 1
             sys.exit(1)
         else:
-            sys.exit(0)
+            # return exit code 0 if the current task has not been passed
+            # but there is still time on the task
+            if task_has_time_left(seconds):
+                sys.exit(0)
+            else:
+                # if there is no time left, return 2
+                sys.exit(2)
     except (OSError, subprocess.CalledProcessError):
         sys.exit(0)
+
+
+def task_has_time_left(seconds):
+    mins_elapsed = int(seconds) / 60.0
+    return mins_elapsed < TASK_TIME_LIMIT
 
 
 def normalize_output(output_path, norm_out_path, task_num):
