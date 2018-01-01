d0a33c6829f707a4500b2972d95366375b1a01ad
Michael Ernst <mernst@cs.washington.edu>
2018-01-01 10:53:38 -0800

    Avoid the need to call chpasswd

--- account_setup/create_accounts.py
+++ account_setup/create_accounts.py
@@ -1,26 +1,32 @@
-""" create_accounts.py creates all the accounts for the study participants on the machine.
+""" create_accounts.py creates accounts, from files usernames.txt and passwords.txt
+
+This is used to create accounts for the study participants.
 """
 
 import subprocess
+import crypt
 
 # get usernames
 usernames = open('usernames.txt').read().strip().split('\n')
 passwords = open('passwords.txt').read().strip().split('\n')
 
-for u,p in zip(usernames, passwords):
+for username,password in zip(usernames, passwords):
 	print "Creating account for", u, "..."
-	cmd = "useradd -m %s" % u
+
+	encrypted_password = crypt.crypt(password,"22") # "22" is the salt
+	cmd = "useradd -m -p %s %s" % (password, username)
 	print subprocess.check_output(cmd.split())
 
-	# set the passwords
-	cmd = "echo %s:%s" % (u, p)
-	p1 = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-	p2 = subprocess.Popen(["chpasswd"], stdin=p1.stdout)
+	## No longer necessary because of use of -p option to useradd
+	# # set the passwords
+	# cmd = "echo %s:%s" % (username, password)
+	# p1 = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+	# p2 = subprocess.Popen(["chpasswd"], stdin=p1.stdout)
 
 	# set default shell to bash
-	cmd = "usermod -s /bin/bash %s" % u
+	cmd = "usermod -s /bin/bash %s" % username
 	print subprocess.check_output(cmd.split())
 
 	# prevent other users from accessing home
-	cmd = "chmod 0700 /home/%s" % u
+	cmd = "chmod 0700 /home/%s" % username
 	print subprocess.check_output(cmd.split())
