63c4db282e0dd90dd9d8742d6f2b110fca058cb8
Michael Ernst <mernst@cs.washington.edu>
2018-01-17 13:54:13 -0800

    Don't be verbose.  Add todo about directory in which this must run.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+# TODO: This script must be run from ~/bash_task_interface.
+# Issue an error if not; otherwise the tar command below will fail.
+
 # copy the exports and aliases to bash_aliases to preserve environment
 # variables in case the user's ssh session is disconnected
 cp bash_aliases.sh $HOME/.bash_aliases
@@ -5,7 +8,7 @@ source $HOME/.bash_aliases
 
 # initializes the task directory
 mkdir $TASK_DIR
-tar -xvzf $REPO_DIR/task_dir.tgz -C $TASK_DIR
+tar -xzf $REPO_DIR/task_dir.tgz -C $TASK_DIR
 
 # move user to the task directory
 cd $TASK_DIR
